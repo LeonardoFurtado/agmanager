bf63bceaa472a97991bad57aa07c25cde382fd8f
Leonardo Furtado <srleonardofurtado@gmail.com>
2024-05-22 22:59:22 -0300

    tests: add tests to list projects.

--- core/tests/test_project.py
+++ core/tests/test_project.py
@@ -1,5 +1,6 @@
 import pytest
 from rest_framework import status
+import logging
 
 
 @pytest.mark.django_db
@@ -40,3 +41,31 @@ def test_should_fail_when_trying_to_change_active_customer_to_inactive_customer(
     response = api_client.patch('/api/projects/1/', data={'customer': inactive_customer.id})
     assert response.status_code == status.HTTP_400_BAD_REQUEST
     assert response.data['customer'][0] == "The customer is not active."
+
+
+@pytest.mark.django_db
+def test_should_fail_when_trying_to_list_projects_from_nonexistent_customer(api_client):
+    response = api_client.get('/api/customers/99/projects/')
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+    assert response.data['detail'] == "Customer not found."
+
+
+@pytest.mark.django_db
+def test_should_list_only_in_progress_projects(api_client, random_project, completed_project):
+    response = api_client.get('/api/projects/?status=in_progress')
+    assert response.status_code == status.HTTP_200_OK
+    assert response.data['count'] == 1
+
+
+@pytest.mark.django_db
+def test_should_list_only_completed_projects(api_client, random_project, completed_project):
+    response = api_client.get('/api/projects/?status=completed')
+    assert response.status_code == status.HTTP_200_OK
+    assert response.data['count'] == 1
+
+
+@pytest.mark.django_db
+def test_should_list_all_progress_if_status_is_blank(api_client, random_project, completed_project):
+    response = api_client.get('/api/projects/?status=')
+    assert response.status_code == status.HTTP_200_OK
+    assert response.data['count'] == 2
