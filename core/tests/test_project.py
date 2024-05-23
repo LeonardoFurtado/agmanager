a65b0f30fa86953df5ac876c35a7fc2f431f23d6
Leonardo Furtado <srleonardofurtado@gmail.com>
2024-05-23 03:32:42 -0300

    tests: add retrieve and delete project tests.

--- core/tests/test_project.py
+++ core/tests/test_project.py
@@ -1,6 +1,5 @@
 import pytest
 from rest_framework import status
-import logging
 
 
 @pytest.mark.django_db
@@ -37,6 +36,21 @@ def test_should_update_project_name(api_client, random_project):
 
 
 @pytest.mark.django_db
+def test_should_update_project_status(api_client, random_project):
+    response = api_client.patch('/api/projects/1/', data={'status': 'completed'})
+    assert response.status_code == status.HTTP_200_OK
+    assert response.data['status'] == "completed"
+
+
+@pytest.mark.django_db
+def test_update_should_fail_if_status_is_not_in_choices(api_client, random_project):
+    response = api_client.patch('/api/projects/1/', data={'status': 'random_status'})
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+    assert 'status' in response.data
+    assert response.data['status'][0] == '"random_status" is not a valid choice.'
+
+
+@pytest.mark.django_db
 def test_should_fail_when_trying_to_change_active_customer_to_inactive_customer(api_client, random_project, inactive_customer):
     response = api_client.patch('/api/projects/1/', data={'customer': inactive_customer.id})
     assert response.status_code == status.HTTP_400_BAD_REQUEST
@@ -65,7 +79,25 @@ def test_should_list_only_completed_projects(api_client, random_project, complet
 
 
 @pytest.mark.django_db
-def test_should_list_all_progress_if_status_is_blank(api_client, random_project, completed_project):
+def test_should_list_all_projects_if_status_is_blank(api_client, random_project, completed_project):
     response = api_client.get('/api/projects/?status=')
     assert response.status_code == status.HTTP_200_OK
     assert response.data['count'] == 2
+
+
+@pytest.mark.django_db
+def test_retrieve_project(api_client, random_project):
+    response = api_client.get('/api/projects/1/')
+    assert response.status_code == status.HTTP_200_OK
+    assert response.data['id'] == random_project.id
+    assert response.data['name'] == random_project.name
+    assert response.data['description'] == random_project.description
+    assert response.data['customer'] == random_project.customer.id
+    assert response.data['status'] == random_project.status
+
+
+@pytest.mark.django_db
+def test_delete_project_by_id(api_client, random_project):
+    response = api_client.delete('/api/projects/1/')
+    assert response.status_code == status.HTTP_204_NO_CONTENT
+    assert response.data is None
